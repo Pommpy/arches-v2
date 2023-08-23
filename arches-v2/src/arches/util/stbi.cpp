0142c72b457dc4595023dfe46fb98612bbfbe7a7
Gaurav Bhokare <89615563+GauravAB92@users.noreply.github.com>
2023-08-23 15:20:49 -0600

    removes .obj files from git ignore

--- arches-v2/src/arches/util/stbi.cpp
+++
@@ -1,8 +0,0 @@
-//stb image
-#define STB_IMAGE_IMPLEMENTATION
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#define STBI_MSC_SECURE_CRT
-
-//stb image
-#include "stb_image.h"
-#include "stb_image_write.h"
